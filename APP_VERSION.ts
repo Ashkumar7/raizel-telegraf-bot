89c2901d2273a2437b6ff5571d52ffa58ca0636e
Raizel #Kreideprinz <Ashishmaharia8@gmail.com>
2023-10-25 22:06:09 +0530

    v1.2.0 Added tunnelAgent Back To handle httpsOverHttp

--- APP_VERSION.ts
+++ APP_VERSION.ts
@@ -1,3 +1,3 @@
-const APP_VERSION = '1.1.1';
+const APP_VERSION = '1.2.0';
 
 export default APP_VERSION;
--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,23 +1,26 @@
 import Axios, { AxiosRequestConfig, AxiosResponse, type AxiosInstance } from 'axios';
 import DotEnv from 'dotenv';
+import * as tunnel from 'tunnel';
 
 /** Load Environment Variables */
 DotEnv.config({ path: '.env.local' });
+
+/** Initialize Tunnel */
+const tunnelAgent = tunnel.httpsOverHttp({
+  proxy: {
+    host: process.env.PROXY_HOST,
+    port: Number(process.env.PROXY_PORT),
+    proxyAuth: `${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}`,
+  },
+});
 
 /** Create a function to create an Axios instance with dynamic types */
 const createAxiosInstance = <T>() => {
   try {
     const axiosInstance: AxiosInstance = Axios.create({
       baseURL: 'https://api.real-debrid.com:443',
-      proxy: {
-        protocol: 'http',
-        host: process.env.PROXY_HOST,
-        port: Number(process.env.PROXY_PORT),
-        auth: {
-          username: process.env.PROXY_USERNAME,
-          password: process.env.PROXY_PASSWORD,
-        },
-      },
+      proxy: false,
+      httpAgent: tunnelAgent,
       headers: {
         Authorization: `Bearer ${process.env.REAL_DEBRID_API_TOKEN}`,
       },
